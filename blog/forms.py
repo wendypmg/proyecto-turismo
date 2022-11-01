7de958919655e0bd37a25c79ffaf67923391edac
Sofia Baili <sofibaili.sb@gmail.com>
2022-10-31 23:19:13 -0300

    agregue los forms

---
+++ blog/forms.py
@@ -0,0 +1,19 @@
+import datetime
+from django import forms
+from django.forms import ModelForm
+from blog.models import Restaurante, Sitio, Monumento
+
+class SitioForm(forms.Form):
+    nombre = forms.CharField(max_length=40, label='Nombre')
+    ciudad= forms.CharField(max_length=100, label='Ciudad')
+    
+class RestauranteForm(forms.Form):
+    nombre = forms.CharField(max_length=40, label='Nombre')
+    ciudad= forms.CharField(max_length=100, label='Ciudad')
+    tipo_de_comida= forms.CharField(max_length=100, label='Tipo de comida')
+
+
+class MonumentoForm(forms.Form):
+    nombre = forms.CharField(max_length=40, label='Nombre')
+    ciudad= forms.CharField(max_length=100, label='Ciudad')
+    fecha= forms.IntegerField( label='Fecha')
