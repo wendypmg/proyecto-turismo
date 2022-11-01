6a66f59ed19847d05f85a206eede9e6f42b51a71
Wendy Manzanero <wendypmg03@gmail.com>
2022-10-31 23:22:19 -0300

    agregamos models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Sitio(models.Model):
+    nombre = models.CharField(max_length=40)
+    ciudad= models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f'{self.nombre} -  {self.ciudad}'
+
+class Restaurante(models.Model):
+    nombre = models.CharField(max_length=40)
+    ciudad= models.CharField(max_length=100)
+    tipo_de_comida= models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.nombre}  - {self.ciudad} - {self.tipo_de_comida}'
+
+class Monumento(models.Model):
+    nombre = models.CharField(max_length=40)
+    ciudad= models.CharField(max_length=100)
+    fecha= models.IntegerField()
+
+    def __str__(self):
+        return f'{self.nombre} - {self.ciudad} - Fecha: {self.fecha}'
+		
+		  
